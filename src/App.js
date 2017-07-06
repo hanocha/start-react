72ec1ca1aea5928e5ead516de12003ab53642294
hanocha <saiki@feedforce.jp>
2017-07-06 23:59:33 +0900

    Add some codes
    Sample of React component, statefull or stateless

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
-const App = () => <h1>hello</h1>
+class App extends React.Component {
+  render() {
+    return <h1>hello with class!</h1>
+  }
+}
+
+// stateless component sample
+// const App = () => <h1>hello with const!</h1>
 
 export default App
