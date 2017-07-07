360a959a16d316318386e3f142ce5b4ac21d413e
hanocha <saiki@feedforce.jp>
2017-07-07 23:56:33 +0900

    Use props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>hello with brackets!</h1>
+        <h1>{this.props.txt}</h1>
         <b>bold text</b>
       </div>
     )
