6fa3278cb1614820c8dd55c30ecc9804994e9016
hanocha <saiki@feedforce.jp>
2017-07-16 12:56:14 +0900

    Remove old codes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,8 @@
 import React from 'react'
 
 class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      text: 'this is a state text',
-    }
-  }
-
-  update(e) {
-    this.setState({
-      text: e.target.value,
-    })
-  }
-
   render() {
-    return (
-      <div>
-        <h1>{this.state.text}</h1>
-        { /* <Widget update={this.update.bind(this)} /> */ }
-        <Widget update={
-            (e) => {
-              this.setState({
-                text: e.target.value,
-              })
-            }
-          } 
-        />
-      </div>
-    )
   }
 }
-
-const Widget = (props) => 
-  <input type="text" onChange={props.update} />
 
 export default App
