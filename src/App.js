51de3ec90c6c75e4fccd196f1b9bdf096f1a5ce6
hanocha <saiki@feedforce.jp>
2017-07-16 13:12:03 +0900

    Add button Component, which use props.children

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 class App extends React.Component {
   render() {
+    return <Button>React</Button>
   }
 }
+
+const Button = (props) => <button>{props.children}</button> 
 
 export default App
