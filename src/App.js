887e989819e8b6e18ad2d2ff9d25ceb884465a27
hanocha <saiki@feedforce.jp>
2017-07-16 21:58:06 +0900

    Fix custom varidator to more better code
    こっちの書き方のほうが何しているのかはわかりやすい気がする

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 class App extends React.Component {
   render() {
-    return <Title />
+    return <Title text='tesT'/>
   }
 }
 
 const Title = (props) => <h1>Title: {props.text}</h1>
 
 Title.propTypes = {
-  text(props, propName, component) {
+  text: function(props, propName, component) {
     if(!(propName in props)) {
       return new Error(`missing prop: ${propName}`)
     }
